3d096ca91cb3f9eb0c176139cb2bb949f91b6d9c
lelabo_m <marc.lelabourier@gmail.com>
2017-11-27 15:00:24 +0100

    Pile: clean, empty, get and see method.

--- src/pile.py
+++ src/pile.py
@@ -6,7 +6,6 @@ class Pile(object):
     def __init__(self):
         self.__members = []
 
-
     @property
     def members(self):
         return self.__members
@@ -14,13 +13,22 @@ class Pile(object):
     def shuffle(self):
         self.__members = random.shuffle(self.__members)
 
-    def push(self, obj):
+    def put(self, obj):
         self.__members.insert(0, obj)
 
-    def get(self, index=0):
+    def get(self):
+        return self.__members.pop(0)
+
+    def see(self, index=0):
         size = len(self.__members)
         if index >= size:
             index = size - 1
         if index < -size:
             index = -size
         return self.__members[index]
+
+    def clean(self):
+        self.__members = []
+
+    def empty(self):
+        return not self.__members
