3658b3392f2536e33e786dabf452126f738c29a0
lelabo_m <marc.lelabourier@gmail.com>
2017-11-27 18:13:58 +0100

    Standard card game implementation.

---
+++ src/card.py
@@ -0,0 +1,21 @@
+from generic import Deck
+
+
+class Card(object):
+
+    def __init__(self, family, value):
+        self.family = family
+        self.value = value
+
+    def __repr__(self):
+        print "{}{}".format(self.value, self.family)
+
+
+class CardDeck(Deck):
+
+    def build(self):
+        families = ["C", "D", "H", "S"]
+        values = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+        for f in families:
+            for v in values:
+                self.stock_stack.put(Card(f, v))
